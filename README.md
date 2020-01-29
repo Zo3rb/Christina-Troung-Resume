4484516b82f96be5cecd5734c87c77ed44e769a7
Ali <forsteve79@gmail.com>
2020-01-29 18:39:23 +0200

    Upload Template

---
+++ README.md
@@ -0,0 +1,15 @@
+# Christina Troung Resume Template HTML/CSS (Not Responsive)
+
+Just Another Template Using HTML / CSS While Learning (Not Responsive)
+
+## Installation
+
+Just Download it and Preview or Check The URL
+
+```
+
+```
+### auther
+```
+https://www.facebook.com/steve.ikognito
+```
---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* global console, alert*/
+
+// The Data Type Tutorial In JavaScript
+
+/*
+
+** The First Type Of Data Type >> ("Boolean - المنطقية" = Takes True Or  False As Value) .
+** The Second Type Of Data Type >> ("Arrays - الصفوف" = Takes As Much As You Want of Values PS. (["Array"], ["Array"]) ).
+** The Third Type Of data Type >> ("Object - موضوع" = Lookis Like arrays but Has Differance PS. ({"object: OB name", "object: OB name"}) ).
+** The Forth Type Of Data Type >> ("String - خيط" = String Name As Text inbetween "Example" ).
+** The Fifth Type Of Data Type >> ("Number - رقم" = Some Number Directly :+n ) .
+*/
+
+var // The Objects Variables
+
+    myPrice = 66665621;
+
+document.getElementById("myPrice").innerHTML = myPrice
